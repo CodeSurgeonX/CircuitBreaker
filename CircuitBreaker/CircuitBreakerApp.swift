1c8582de88425437948ca1671ba7b8b6ee19c053
Shashwat Kashyap <shashwat.kashyap@icloud.com>
2024-03-03 16:31:22 +0530

    Initial Commit

---
+++ CircuitBreaker/CircuitBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircuitBreakerApp.swift
+//  CircuitBreaker
+//
+//  Created by Shashwat Kashyap on 03/03/24.
+//
+
+import SwiftUI
+
+@main
+struct CircuitBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
